68deabf56527e7454bbe1b9761ac6bf5c6c667c6
John Vincent Panes <janvincent@wylog.com>
2021-02-22 01:13:13 +0800

    Add update

--- js/custom.js
+++ js/custom.js
@@ -1,19 +1,29 @@
-$(document).ready(function(){
+$(document).ready(function () {
   AOS.init();
   // Sidebar Functions
-  $("#open_sidebar").click( function() {
-    $("#sidebar-navigation").addClass("open")
-  })
+  $("#open_sidebar").click(function () {
+    $("#sidebar-navigation").addClass("open");
+  });
 
-  $("#close_sidebar").click( function() {
-    $("#sidebar-navigation").removeClass("open")
-  })
+  $("#close_sidebar").click(function () {
+    $("#sidebar-navigation").removeClass("open");
+  });
   // Outside element click trigger
   $("html").click(function (e) {
-    if (e.target.id == 'sidebar-navigation' || e.target.id == 'open_sidebar') {
+    if (e.target.id == "sidebar-navigation" || e.target.id == "open_sidebar") {
       // Do nothing
     } else {
-      $("#sidebar-navigation").removeClass("open")
+      $("#sidebar-navigation").removeClass("open");
     }
-  })
+  });
+
+  $("#read-more").click(function () {
+    var text = $(this).html();
+    if (text === "READ MORE") {
+      $(this).html("READ LESS");
+    } else {
+      $(this).html("READ MORE");
+    }
+    $("#more-content").toggleClass("more");
+  });
 });
